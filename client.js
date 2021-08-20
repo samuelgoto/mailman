de9f425f8411a8b729e5850cc344b6a375f2aeb1
Samuel Goto <samuelgoto@gmail.com>
2021-08-20 09:41:23 -0700

    Kick off a build

--- client.js
+++ client.js
@@ -1,5 +1,10 @@
 const nodemailer = require("nodemailer");
 const dns = require("dns");
+const smime = require('nodemailer-smime');
+var fs = require("fs");
+
+// Creating SMIME certificates
+// https://serverfault.com/questions/103263/can-i-create-my-own-s-mime-certificate-for-email-encryption
 
 async function send(email) {
   const domain = email.to.split("@")[1];
@@ -14,11 +19,25 @@ async function send(email) {
     });
   });
 
+  // console.log(host);
+
   const transporter = nodemailer.createTransport({
     host: host,
     port: 25,
+    // port: 465,
+    // port: 587,
   });
 
+  // console.log(fs.readFileSync("humble_coder.key", "utf8"));
+
+  //transporter.use("stream", smime({
+  //  cert: fs.readFileSync("humble_coder.crt", "utf8"),
+  //  chain: [
+  //    fs.readFileSync("ca.crt", "utf8")
+  //  ],
+  //  key: fs.readFileSync("humble_coder.key", "utf8")
+  //}));
+  
   const result = await new Promise((resolve, reject) => {
     transporter.sendMail(email, function(error, info){
       if (error) {
