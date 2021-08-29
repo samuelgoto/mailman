f2a617199b03b8e3fbff13b581aa4f4a9c04ad71
Samuel Goto <samuelgoto@gmail.com>
2021-08-29 09:23:32 -0700

    Add tests for the generation of onion service ids

---
+++ tests/onion.js
@@ -0,0 +1,19 @@
+const {
+  createNewServiceKey,
+  onion_public_key_from_expanded_key,
+  onion_service_address_for_public_key,
+  onion_service_public_key_form_id
+} = require("p2pnap/lib/util.js")
+
+const Assert = require("assert");
+
+describe("onion", () => {
+  it("v3 addresses", async () => {
+    const secretKey = createNewServiceKey();
+    const pubKey = onion_public_key_from_expanded_key(secretKey);
+    const serviceId = onion_service_address_for_public_key(pubKey);
+    Assert.deepEqual(
+      pubKey,
+      onion_service_public_key_form_id(serviceId));
+  });
+});
